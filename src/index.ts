e24c9e55911ea3f6213c1b6835eb9cf109ae0e52
Cristian Guzmán <cristian.guzman.contacto@gmail.com>
2026-05-06 19:07:50 -0600

    🐛 small fixes

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,2 @@
-export { verifyTokenMiddleware } from './Middlewares/verifyTokenMiddleware';
-export { Token } from './Services/Token';
+export { verifyTokenMiddleware } from './Middlewares/verifyTokenMiddleware.js';
+export { Token } from './Services/Token.js';
